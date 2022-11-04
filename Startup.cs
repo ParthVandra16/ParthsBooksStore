1906fe0ca36b7284b54e11325d3f1ab368bda621
ParthVandra16 <106701842+ParthVandra16@users.noreply.github.com>
2022-11-04 11:08:17 -0400

    last step cannot run

--- Startup.cs
+++ Startup.cs
@@ -66,7 +66,7 @@ namespace ParthsBooksStore
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customers}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
