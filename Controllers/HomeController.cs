47d44d703ee59b4e2198abf843665be2ccd096e9
ParthVandra16 <106701842+ParthVandra16@users.noreply.github.com>
2022-11-04 10:59:14 -0400

    solve error in startup.cs and modify

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -31,7 +31,7 @@ namespace ParthsBooksStore.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ParthsBooksStore.Models.ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
